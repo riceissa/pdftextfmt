53c98596e71dc826cd8b6a7be6de2807a19e09e1
Issa Rice <riceissa@gmail.com>
2019-01-15 08:15:28 -0800

    i think this fixes hyphen on own line
    Closes #3.

--- pdftextfmt.go
+++ pdftextfmt.go
@@ -11,9 +11,25 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	result := ""
+	var joinNextLine = false
 	for scanner.Scan() {
 		text := scanner.Text()
-		text = text + " "
+		if !joinNextLine {
+			text = text + " "
+		}
+
+		// If the whole line is just a hyphen, we want to interpret
+		// this as part of a word so join it with the following line.
+		// An example would be something like ["mathe", "-",
+		// "matician"].
+		matched, _ := regexp.MatchString("^[[:space:]]*-[[:space:]]*$", text)
+		if matched {
+			result = strings.TrimSpace(result)
+			joinNextLine = true
+			continue
+		} else {
+			joinNextLine = false
+		}
 
 		re := regexp.MustCompile("-[[:space:]]+$")
 		text = re.ReplaceAllString(text, "")
